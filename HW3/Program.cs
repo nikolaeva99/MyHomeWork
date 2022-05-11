d4fbfd34b8d926bc75c1d8d0266807856505cf8f
Anastasia <nsta_nikolaeva@inbox.ru>
2022-05-11 12:41:55 +0300

    Homework3

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -3,8 +3,31 @@
 // 12821 -> да
 // 23432 -> да
 
+void Palindrome (int a)
+{
+    int lastNum = a % 10;
+    int firstNum = a / 10000;
+    int preLastNum = (a % 100) / 10;
+    int secondNum = (a / 1000) % 10;
 
+    if (a / 100000 >= 1 | a / 10000 < 0.1)
+    Console.WriteLine("Input 5-digit number!");
+    else 
+        if (lastNum == firstNum && preLastNum ==secondNum)
+        {
+            Console.WriteLine ("The number is palindrome");
+        }
+        else 
+        {
+            Console.WriteLine ("The number is NOT palindrome");
+        }
+}
 
+Console.Write ("Input 5-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Palindrome (num);
+
+/*
 // Задача 2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // А (3, 6, 8); В (2, 1, -7) -> 15.84
 // А (7, -5, 0); В (1, -1, 9) -> 11.53
@@ -30,7 +53,7 @@ Console.Write ("Input Bz coordinate: ");
 double dotBz = Convert.ToDouble(Console.ReadLine());
 
 LengthFind (dotAx, dotAy, dotAz, dotBx, dotBy, dotBz);
-
+*/
 
 
 
@@ -54,3 +77,4 @@ Console.Write ("Input integer number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 TableCube(num);
 */
+
