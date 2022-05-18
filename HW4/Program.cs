5460151194f37d826872e3274d5fa3be11bcac24
Anastasia <nsta_nikolaeva@inbox.ru>
2022-05-18 14:21:34 +0300

    Task 4

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -1,7 +1,8 @@
-﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+﻿// Задача1. Задайте массив заполненный случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+/*
 int[] PrintArray (int size)
 {
     int[] array = new int[size];
@@ -12,7 +13,6 @@ int[] PrintArray (int size)
     return array;
 }
 
-
 void ShowArray (int[] array)
 {
     for (int i = 0; i < array.Length; i++)
@@ -21,7 +21,7 @@ void ShowArray (int[] array)
     Console.WriteLine();
 }
 
-void ShowPositiveNum (int[] array)
+void ShowEvenNum (int[] array)
 {
     int current = 0;
     for (int i = 0; i < array.Length; i++)
@@ -34,4 +34,102 @@ void ShowPositiveNum (int[] array)
 
 int[] newArray = PrintArray(4);
 ShowArray(newArray);
-ShowPositiveNum(newArray);
+ShowEvenNum(newArray);
+*/
+
+
+// Задача 2. Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+/*
+
+int[] CreateArray (int size, int min, int max)
+{
+    int[] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void ShowArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+void SumEvenNumber (int[] array)
+{
+    int sumEven = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 0) sumEven = sumEven + array[i];
+  
+    }
+    Console.WriteLine(sumEven);
+}
+
+int[] myArray = CreateArray(4, 0, 100);
+ShowArray(myArray);
+SumEvenNumber(myArray);
+*/
+
+/*
+// Задача 3. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] newArray (int size)
+{
+    double[] array = new double [size];
+    for (int i = 0; i < size; i++)
+    {
+        array [i] = new Random().Next (-1000, 1000);
+    }
+    return array;
+}
+
+void ShowArray (double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+        Console.WriteLine();
+}
+
+double FindMax (double[] array)
+{
+    double max = array[1];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+double FindMin (double[] array)
+{
+    double min = array[1];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+double[] myArray = newArray(8);
+ShowArray(myArray);
+Console.WriteLine("Max number is " + FindMax(myArray));
+Console.WriteLine ("Min number is " + FindMin(myArray));
+double residual = FindMax(myArray) - FindMin(myArray);
+Console.WriteLine("Residual between max and min is: " + residual);
+*/
